ad238eb83027bf4a2a3373a54fe0d4f27e8e5b36
Khubaib Jamal <khubaib.j18@gmail.com>
2023-10-30 23:34:06 +0500

    adding flushbar

---
+++ lib/utils/utils.dart
@@ -0,0 +1,37 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+  static void flutterToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      gravity: ToastGravity.BOTTOM,
+      fontSize: 16,
+    );
+  }
+
+  // another flush bar
+  static void flushBar(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        // title: "Something went wrong",
+        message: message,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        icon: const Icon(Icons.error, size: 30, color: Colors.white),
+        backgroundColor: Colors.black,
+        isDismissible: true,
+        borderRadius: BorderRadius.circular(30),
+        dismissDirection: FlushbarDismissDirection.HORIZONTAL,
+        flushbarPosition: FlushbarPosition.TOP,
+        duration: const Duration(seconds: 3),
+        reverseAnimationCurve: Curves.easeInOut,
+        forwardAnimationCurve: Curves.decelerate,
+      )..show(context),
+    );
+  }
+}
