66ab197a10a018dff28606f6bcb73544f2a859dd
Khubaib Jamal <khubaib.j18@gmail.com>
2023-11-08 14:39:44 +0500

    get set user  data from shared preferences

---
+++ lib/view_model/user_view_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../model/user_model.dart';
+
+class UserViewModel with ChangeNotifier {
+  // save user token
+  Future<bool> saveUser(UserModel user) async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setString("token", user.token.toString());
+    notifyListeners();
+    return true;
+  }
+
+  // get user token
+  Future<UserModel> getUser() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    final String? token = sp.getString('token');
+    return UserModel(token: token);
+  }
+
+  // clear user
+  Future<bool> remove() async {
+    final SharedPreferences sp = await SharedPreferences.getInstance();
+    return sp.clear();
+  }
+}
